25d12d2b4ca656494689dc32a3a600c3d3b03814
Judy <judys717@hotmail.com>
2019-10-26 04:42:48 -0400

    Added materialize style

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var express = require("express");
 var exphbs = require("express-handlebars");
-var mongojs = require("mongoose");
+var mongoose = require("mongoose");
 
 var db = "./models";
 
@@ -16,6 +16,15 @@ app.use(express.static("public"));
 app.set("view engine", "handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
 
 app.listen(PORT, function() {
   const HOST = process.env.HOST || "http://localhost";
